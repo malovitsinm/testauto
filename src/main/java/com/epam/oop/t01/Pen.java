e1fed18307d10e7d028a64ce858a73bda5bd4c05
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-12-14 16:38:48 +0300

    Initial commit. Added OOP Task01

---
+++ src/main/java/com/epam/oop/t01/Pen.java
@@ -0,0 +1,43 @@
+package com.epam.oop.t01;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class Pen {
+    private String inkColor;
+    private String inkType;
+
+    @Override
+    public String toString() {
+        return "Pen{" +
+                "inkColor='" + inkColor + '\'' +
+                ", inkType='" + inkType + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pen pen = (Pen) o;
+
+        if (inkColor != null ? !inkColor.equals(pen.inkColor) : pen.inkColor != null) return false;
+        return inkType != null ? inkType.equals(pen.inkType) : pen.inkType == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = inkColor != null ? inkColor.hashCode() : 0;
+        result = 31 * result + (inkType != null ? inkType.hashCode() : 0);
+        return result;
+    }
+
+    public String writeSomething(String text){
+        return text + " This text is written in " + inkColor + " color.";
+    }
+}
