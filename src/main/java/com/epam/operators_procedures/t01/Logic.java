0434a5a26b462c20e26fc6a99ffca9cb2aae9ec9
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-12-14 16:42:25 +0300

    Added Operators&Procedures Task01 code.

---
+++ src/main/java/com/epam/operators_procedures/t01/Logic.java
@@ -0,0 +1,7 @@
+package com.epam.operators_procedures.t01;
+
+public class Logic {
+    public String method() {
+        return "I am string in Logic.";
+    }
+}
---
+++ src/main/java/com/epam/operators_procedures/t01/Main.java
@@ -0,0 +1,8 @@
+package com.epam.operators_procedures.t01;
+
+public class Main {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
