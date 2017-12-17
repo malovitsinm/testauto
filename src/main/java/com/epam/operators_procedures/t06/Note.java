def87fa3cf9d2680a166d5fec7399cae8eefda31
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-12-17 21:00:07 +0300

    Added javadoc for Project Lombok usage.

--- src/main/java/com/epam/operators_procedures/t06/Note.java
+++ src/main/java/com/epam/operators_procedures/t06/Note.java
@@ -4,7 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
+/**
+ * In order to solve the problem of boiler-plate code this class uses Project Lombok annotation processor.
+ * For more details about this tool please refer to project Lombok
+ * <a href='https://projectlombok.org/features/all">feature page</a>.
+ *
+ * */
 @Setter
 @Getter
 @AllArgsConstructor
