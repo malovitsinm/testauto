3d172f974523358cb4df871cf9a01dc14ba209f1
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2017-12-18 09:49:21 +0300

    Fixed bugs caught by working with "IEEE 754" values.

--- src/main/java/com/epam/operators_procedures/t04/LinearArray.java
+++ src/main/java/com/epam/operators_procedures/t04/LinearArray.java
@@ -4,10 +4,15 @@ public class LinearArray {
     public double findMaximum(double[] array) {
         double maximum = 0.0;
         for (int i = 0; i < array.length / 2; i++) {
-            maximum = Math.max(
-                    maximum,
-                    array[i] + array[array.length - 1 - i]
-            );
+            int index = array.length - 1 - i;
+            if (Double.compare(array[i], Double.MAX_VALUE) == 0
+                    || Double.compare(array[index], Double.MAX_VALUE) == 0) maximum = Double.MAX_VALUE;
+            else if (Double.compare(array[i], Double.MIN_VALUE) == 0
+                    || Double.compare(array[index], Double.MIN_VALUE) == 0) continue;
+            else maximum = Math.max(
+                        maximum,
+                        array[i] + array[index]
+                );
         }
         if (array.length / 2 == 1) maximum = Math.max(maximum, array[array.length / 2 + 1]);
         return maximum;
